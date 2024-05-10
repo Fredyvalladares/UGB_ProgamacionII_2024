c9ec2c36daaea35546fe63b5b989a05deee6b37c
Fredy Valladares <144206158+Fredyvalladares@users.noreply.github.com>
2024-05-10 05:34:42 +0000

    Unidad 4 C# para Linux_Ejercicio02

---
+++ ejercicio02.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Unidad_4_ISNP152823_ISNP088623
+{
+    public static void Main(string[] args)
+    {
+        int suma = 0; // Declarar e inicializar suma
+        int num; // Declarar variable num
+
+        do
+        {
+            Console.Write("Ingrese un Numero Positivo Para Sumar: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                suma += num;
+            }
+        }
+        while (num > 0);
+
+        Console.WriteLine();
+        Console.WriteLine("La Suma de los Numeros Positivos es: " + suma);
+        Console.WriteLine();
+        Console.WriteLine("Presiona Cualquier Tecla para Salir");
+        Console.ReadKey();
+    }
+}

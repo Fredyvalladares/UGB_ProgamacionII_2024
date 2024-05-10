cef71f05829d4f9d37458b8fde79a6ef28667c5c
Fredy Valladares <144206158+Fredyvalladares@users.noreply.github.com>
2024-05-10 04:52:44 +0000

    Unidad 4 C# para Linux_Ejercicio01

---
+++ ejercicio01.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Unidad_4_ISNP152823_ISNP088623
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite su nombre: ");
+        string nombre = Console.ReadLine();
+        if (nombre == "Fredy") 
+        {
+            Console.WriteLine("Usted está Autorizado");
+            Console.WriteLine("Hola {0}", nombre);
+        }
+        else
+        {
+            Console.WriteLine("No te conozco {0}", nombre); 
+    }
+}
